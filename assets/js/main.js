fecfb0a0dd26ffd593240fdf657687b040f3c225
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-02-20 23:17:06 -0800

    app 2

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+jQuery(function ($) {
+
+
+  function onScroll(event){
+			//add class active when click on navbar item
+		var scrollPosition = $(document).scrollTop();
+		$('.navbar-collapse .nav-item a[href^="#"').each(function () {
+			console.log($(this).attr('href')); 
+		  var refElement = $($(this).attr("href"));
+		  if (refElement.length&&refElement.position().top <= scrollPosition) {
+			$('.navbar-collapse .nav-item ').removeClass("active");
+			$(this).parent().addClass("active");
+		  }
+		  else{
+			$(this).removeClass("active");
+		  }
+  
+		});
+	  }
+    $(document).on("scroll", onScroll);
+//animate to section when click
+    $('.navbar-collapse ul li a').on('click', function () {
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top  - 5 }, 1000);
+      $('.navbar-collapse.collapse.in').removeClass('in');
+      return false;
+    });
+        // add fix navbar when scroll down and remove it on top 
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+
+    if (scrollTop > $('header').height()) {
+      $('header').addClass('scrollNav');
+
+    }
+
+    else {
+      $('header').removeClass('scrollNav');
+    }
+
+  });
+
+    
+  //close mobile navbar when chooe one of items
+  $(".navbar .nav-link").click(function(){
+    $(".collapse").removeClass("show");
+  });
+  $('.workSlider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    margin: 20,
+    nav: false,
+    rtl:true,
+    dots:true,
+    responsive: {
+      0: {
+        items: 1,
+
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+
+      }
+      
+
+    }
+  });
+  
+});
+
